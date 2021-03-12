2eb6c5c7a423ce069959b61dfc422d2e031fcfdd
Christopher Ortega <A01634500@itesm.mx>
2021-03-11 19:22:30 -0700

    some changes

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,12 +1,11 @@
 
 const admin = require("firebase-admin");
-const { Storage } = require("@google-cloud/storage");
 const serviceAccount = require("./iknelia-3cd8e-firebase-adminsdk-tvnqs-e15e084561.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
-const storage = new Storage(serviceAccount);
+const storage = admin.storage();
 
 module.exports = { admin, storage };
