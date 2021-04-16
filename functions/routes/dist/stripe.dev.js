70b645ee4b487655f9d6243e71e416663b943e8b
Christopher Ortega <A01634500@itesm.mx>
2021-04-16 13:11:12 -0600

    du

--- functions/routes/dist/stripe.dev.js
+++ functions/routes/dist/stripe.dev.js
@@ -27,23 +27,33 @@ exports.sendPaymentInfo = function (req, res) {
     })["catch"](function (er) {
       console.error(er.message);
     });
-  });
-  stripe.paymentIntents.create({
-    "amount": amount,
-    "currency": 'mxn',
-    "description": 'Sesión individual',
-    "payment_method": payment_method_id,
-    "payment_method_types": ['card', 'oxxo'],
-    "receipt_email": email
-  }).then(function (paymentIntent) {
-    console.log(paymentIntent.receipt_email, "Ticket de pago generado exitosamente api");
-    return res.status(200).send({
-      "client_secret": paymentIntent.client_secret,
-      "message": 'Ticket de pago generato exitosamente'
+    thers.doc(doc.data().therapist).get().then(function (ther) {
+      console.log();
+      stripe.paymentIntents.create({
+        "amount": amount,
+        "currency": 'mxn',
+        "description": 'Sesión individual',
+        "payment_method": payment_method_id,
+        "application_fee_amount": 10000,
+        "transfer_data": {
+          "destination": ther.data().stripeId
+        },
+        "payment_method_types": ['card', 'oxxo'],
+        "receipt_email": email
+      }).then(function (paymentIntent) {
+        console.log(paymentIntent.receipt_email, "Ticket de pago generado exitosamente api");
+        return res.status(200).send({
+          "client_secret": paymentIntent.client_secret,
+          "message": 'Ticket de pago generato exitosamente'
+        });
+      })["catch"](function (error) {
+        console.log('Error al crear el intento de pago', error.message);
+        res.status(400).send(error);
+      });
+    })["catch"](function (error) {
+      console.log('Error al obtener los datos de el terapeuta del usuario', error.message);
+      res.status(400).send(error);
     });
-  })["catch"](function (error) {
-    console.log('Error al procesar tu pago', error.message);
-    res.status(400).send(error);
   });
 };
 
